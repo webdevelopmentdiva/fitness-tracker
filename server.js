89991a4ed19763949b950c0e1caf932809334265
Skyler Bluth <skylerbluth@Skylers-MacBook.local>
2020-06-26 14:09:01 -0700

    mispelled

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Workout = require("./models/workouts");
 
 const PORT = process.env.PORT || 3030;
 
-mongoose.connect(process.env.MOGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
 });
 
